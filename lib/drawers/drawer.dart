82374e7ae6ebcdb7bddac7b343e576a186def8ac
ashup <ashupal0089@gmail.com>
2024-02-21 20:12:46 +0530

    21-02-2024

---
+++ lib/drawers/drawer.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class drawer extends StatefulWidget {
+  const drawer({super.key});
+
+  @override
+  State<drawer> createState() => _drawerState();
+}
+
+class _drawerState extends State<drawer> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
